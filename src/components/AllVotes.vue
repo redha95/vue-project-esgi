8f4e4e9590c30f7857f7964658037595ace63d62
Dylan Settbon <dylans_set@hotmail.fr>
2019-12-04 12:36:06 +0100

    [FIX] Component Vote

--- src/components/AllVotes.vue
+++ src/components/AllVotes.vue
@@ -1,14 +1,14 @@
 <template>
  <div class=" container mx-auto py-10">
-		<div class="border m-6 rounded-lg  bg-white shadow-lg rounded-lg overflow-hidden">
+		<div class="border m-6 rounded-lg  bg-white shadow-xl rounded-lg overflow-hidden">
 			<div class="sm:flex sm:items-center px-6 py-4">
 					<div class="text-center sm:text-left sm:flex-grow">
 						<div class="mb-4">
-							<p class="text-xl leading-tight">Titre</p>
-							<p class="text-sm leading-tight text-grey-dark">Description</p>
+							<p class="text-3xl leading-tight mb-3">Titre</p>
+							<p class="text-base leading-tight text-grey-dark">Description</p>
 						</div>
-						<div class="flex flex-wrap">
-							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Voir Plus</button>
+						<div class="flex justify-center flex-wrap mt-6">
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple">Voir Plus</button>
 						</div>
 					</div>
 			</div>
