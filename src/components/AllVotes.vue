3c4a59ea1c5ca27122bab5429337c9a1f46cd069
Redha <--replace-al>
2019-12-27 14:53:29 +0100

    ajout donnes factices, affichage vote by id

--- src/components/AllVotes.vue
+++ src/components/AllVotes.vue
@@ -1,24 +1,37 @@
 <template>
+<ul>
+<li v-for="list in lists" v-bind:key="list.id" v-bind:list="list">
  <div class=" container mx-auto py-10">
 		<div class="border m-6 rounded-lg  bg-white shadow-xl rounded-lg overflow-hidden">
 			<div class="sm:flex sm:items-center px-6 py-4">
 					<div class="text-center sm:text-left sm:flex-grow">
 						<div class="mb-4">
-							<p class="text-3xl leading-tight mb-3">Titre</p>
-							<p class="text-base leading-tight text-grey-dark">Description</p>
+							<p class="text-3xl leading-tight mb-3">{{list.title}}</p>
+							<p class="text-base leading-tight text-grey-dark">{{list.description}}</p>
 						</div>
 						<div class="flex justify-center flex-wrap mt-6">
-							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple"><a href="/showVote">Voir Plus</a></button>
+							<button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple"><router-link :to="{ name: 'VoteDetails', params: { list } }">Voir Plus </router-link></button>
 						</div>
-					</div>
+				</div>
 			</div>
 		</div>
  </div>
+</li>
+</ul>
 </template>
 
 <script>
 export default {
-    name: "AllVotes"
+	name: "AllVotes",
+	data: () => ({
+        lists: [
+            {title:"Le meilleur buteur", description:"Votez pour le meilleur buteur de ligue 1 2020 !",author:"Redha AISSAOUI",createdAt:"10/10/2010"},
+            {title:"Fraise ou banane?", description:"Aidez nous a faire un choix, votez pour votre fruit préferé",author:"Redha AISSAOUI",createdAt:"27/12/2019"},
+            {title:"Golf au audi", description:"jfrijfirfjrijfrijrfijfrjfrijfrijfrijfijrfjri",author:"Redha AISSAOUI",createdAt:"01/02/2019"},
+            {title:"Golang ou VueJS", description:"quel est le meilleur framework",author:"Redha AISSAOUI",createdAt:"11/11/2011"},
+        ],
+        name: "Detail d'un vote"
+    })
 }
 </script>
 
--- src/components/VoteDetails.vue
+++ src/components/VoteDetails.vue
@@ -1,26 +1,32 @@
 <template>
- <div class="max-w-md w-full lg:flex">
-  <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('https://tailwindcss.com/img/card-left.jpg')" title="Woman holding a mug">
-  </div>
-  <div class="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-    <div class="mb-8">
-      <p class="text-sm text-grey-dark flex items-center">
-        <svg class="text-grey w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-        </svg>
-        Members only
-      </p>
-      <div class="text-black font-bold text-xl mb-2">Can coffee make you a better developer?</div>
-      <p class="text-grey-darker text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+<div class="mx-auto  h-screen flex items-center justify-center px-8">
+  <div class="flex flex-col w-full bg-white rounded shadow-lg sm:w-3/4 md:w-1/2 lg:w-3/5">
+    <div class="w-full h-64 bg-top bg-cover rounded-t" style="background-image: url(https://www.si.com/.image/t_share/MTY4MTkyMjczODM4OTc0ODQ5/cfp-trophy-deitschjpg.jpg)"></div>
+    <div class="flex flex-col w-full md:flex-row">
+        <div class="flex flex-row justify-around p-4 font-bold leading-none text-gray-800 uppercase bg-gray-400 rounded md:flex-col md:items-center md:justify-center md:w-1/4">
+          
+            <div class="md:text-3xl">{{$route.params.list.createdAt}}</div>
+        </div>
+        <div class="p-4 font-normal text-gray-800 md:w-3/4">
+            <h1 class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-800">{{ $route.params.list.title}}</h1>
+            <p class="leading-normal">{{ $route.params.list.description}}</p>
+            <div class="flex flex-row items-center mt-4 text-gray-700">
+                <div class="w-1/2">
+                    Créer par : {{ $route.params.list.author}}
+                </div>
+                <div class="w-1/2 flex justify-end">
+                       <a href="/allVotes"> <button class="auth-button border block rounded-sm font-bold py-4 px-6 mr-2 flex items-center hover:text-white">
+                          <svg class="h-5 w-5 mr-2 fill-current" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-49 141 512 512" style="enable-background:new -49 141 512 512;" xml:space="preserve">
+                              <path id="XMLID_10_" d="M438,372H36.355l72.822-72.822c9.763-9.763,9.763-25.592,0-35.355c-9.763-9.764-25.593-9.762-35.355,0 l-115.5,115.5C-46.366,384.01-49,390.369-49,397s2.634,12.989,7.322,17.678l115.5,115.5c9.763,9.762,25.593,9.763,35.355,0 c9.763-9.763,9.763-25.592,0-35.355L36.355,422H438c13.808,0,25-11.193,25-25S451.808,372,438,372z"></path>
+                          </svg>
+                          Retour
+                        </button>
+                        </a>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="flex items-center">
-      <img class="w-10 h-10 rounded-full mr-4" src="https://pbs.twimg.com/profile_images/885868801232961537/b1F6H4KC_400x400.jpg" alt="Avatar of Jonathan Reinink">
-      <div class="text-sm">
-        <p class="text-black leading-none">Jonathan Reinink</p>
-        <p class="text-grey-dark">Aug 18</p>
-      </div>
-    </div>
-  </div>
+</div>
 </div>
 </template>
 
@@ -29,7 +35,14 @@ export default {
     name: "VoteDetails"
 }
 </script>
-
 <style>
+.auth-button{
+    color: #5ab79b;
+    border:1px solid #5ab79b;
+}
 
+.auth-button:hover{
+    background-color: #5ab79b;
+    color:white;
+}
 </style>
